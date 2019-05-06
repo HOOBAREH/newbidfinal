88318222e7e36d8ffecd35db62c79b1aa88b5013
zahra-hvn <zahrahavadanloo@gmail.com>
2019-05-06 14:39:52 +0430

    chnages datalayer

---
+++ DataLayer/Model/AuctionModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataLayer.Model
+{
+    public class AuctionListModel
+    {
+        List<AuctionModel> Auctions = new List<AuctionModel>();
+    }
+    public class AuctionModel
+    {
+        public int ProductId { get; set; }
+        public int id { get; set; }
+        public TimeSpan Auction_Time { get; set; }
+        public int Close_Time { get; set; }
+        public int Reserve_Price { get; set; }
+        public int EndPrice { get; set; }
+        public int CurrentBid_Id { get; set; }
+        public int Current_UserId { get; set; }
+        public int Current_UserName { get; set; }
+    }
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+    }
+}
