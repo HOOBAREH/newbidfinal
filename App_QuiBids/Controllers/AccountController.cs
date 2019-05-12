9d1bc52743ef6d17aa9455f7d2343d187a9a3502
AmirHosein-G <50169656+AmirHosein-G@users.noreply.github.com>
2019-05-12 03:52:31 -0700

    Account Controller

---
+++ App_QuiBids/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace App_QuiBids.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Account
+        public ActionResult MyGames()
+        {
+            return View();
+        }
+        public ActionResult NewBids()
+        {
+            return View();
+        }
+        public ActionResult MyAccount()
+        {
+            return View();
+        }
+        public ActionResult MyBadges()
+        {
+            return View();
+        }
+    }
+}
